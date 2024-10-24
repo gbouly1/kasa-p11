2ff0061000144ca33613892e2770776411504e6c
Guillaume Bouly <gbouly1@gmail.com>
2024-10-24 20:46:32 +0200

    banner about

--- kasa/src/pages/about/About.jsx
+++ kasa/src/pages/about/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./about.css"
+import Banner from '../../components/banner/Banner'
+import homeBannerImage from "../../assets/img/banner-about.png"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-container">
+
+    <div className="about-hero-banner">
+      <Banner 
+      image={homeBannerImage} />
+    </div>
+
+
+    </div>
   )
 }
 
--- kasa/src/pages/logement/Logement.jsx
+++ kasa/src/pages/logement/Logement.jsx
@@ -6,12 +6,14 @@ import { useParams } from 'react-router-dom';
 const Logement = () => {
   const { id } = useParams()
   const logement = data.find((logement) => logement.id === id)
+
   if (!logement) {
     return <h2>Logement non trouvé</h2>
   }
 
   return (
     <h1>{logement.title}</h1>
+
   )
 }
 
