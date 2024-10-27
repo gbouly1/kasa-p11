30dbcb6f7b8713a31f23ce66bbf59d21ddfd8b3a
Guillaume Bouly <gbouly1@gmail.com>
2024-10-27 15:59:22 +0100

    page error 404

--- kasa/src/pages/error/Error.jsx
+++ kasa/src/pages/error/Error.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import "./error.css"
+import React from "react";
+import "./error.css";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className="error-container">
+      <h2 className="error-404">404</h2>
+      <p className="page-error">Oups! La page que vous demandez n'existe pas</p>
+      <Link to={`/`} className="back-home">
+        <p className="back-content">Retourner à la page d'accueil</p>
+      </Link>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
